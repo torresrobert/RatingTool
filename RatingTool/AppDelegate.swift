495521071a515f502825c924c561d54a2a02f0ae
Robert <torresrobert@me.com>
2017-12-02 08:55:10 -0500

    Working database, working log in functionality. All locations now available but need to correctly filter the comments to their corresponding location.

--- RatingTool/AppDelegate.swift
+++ RatingTool/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- RatingTool/FirstViewController.swift
+++ RatingTool/FirstViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
+
 //favorites
 var fav1 = "Theatres"
 var fav2 = "Coffee"
 var fav3 = "Restaurants"
 var fav4 = "Parks and Rec"
 var fav5 = "Fast Food"
+
+var ref: DatabaseReference!
 
 struct Users{
     var email = ""
@@ -32,18 +38,53 @@ class FirstViewController: UIViewController {
     @IBOutlet var login: UIButton!
     
     @IBAction func attempt_login_email(_ sender: Any) {
-        if(robert.email==input_email.text&&robert.password==input_password.text){
-            performSegue(withIdentifier: "seque", sender: self)
-        }
-        else{
-           // EXIT_FAILURE
+            ref = Database.database().reference()
+        
+        if self.input_email.text == "" || self.input_password.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+        Auth.auth().signIn(withEmail: self.input_email.text!, password: self.input_password.text!) { (user, error) in
+            
+            if error == nil {
+                
+                //Print into the console if successfully logged in
+                print("You have successfully logged in")
+                
+                //Go to the HomeViewController if the login is sucessful
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                self.present(vc!, animated: true, completion: nil)
+            }
+            else {
+                
+                //Tells the user that there is an error and then gets firebase to tell them the error
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+
+            }
         }
         
     }
+
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        login.layer.cornerRadius=10
+       // login.layer.cornerRadius=10
     }
 
     override func didReceiveMemoryWarning() {
